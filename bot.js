193a4eefa206bed80fcf844d3d50f8d7e86cd669
ooglyboogly <drlucas17@yahoo.com>
2017-07-15 21:19:05 -0500

    whatever

--- bot.js
+++ bot.js
@@ -42,59 +42,7 @@ fs.readdirSync(normalizedPath).forEach(file => {
   require(path.join(normalizedPath, file))(controller)
 })
 
-controller.hears(['shirt'],'message_received',function(bot, message) {
-    bot.reply(message, {
-        attachment: {
-            'type':'template',
-            'payload':{
-                 'template_type':'generic',
-                 'elements':[
-                   {
-                     'title':'Classic White T-Shirt',
-                     'image_url':'http://petersapparel.parseapp.com/img/item100-thumb.png',
-                     'subtitle':'Soft white cotton t-shirt is back in style',
-                     'buttons':[
-                       {
-                         'type':'web_url',
-                         'url':'https://petersapparel.parseapp.com/view_item?item_id=100',
-                         'title':'View Item'
-                       },
-                       {
-                         'type':'web_url',
-                         'url':'https://petersapparel.parseapp.com/buy_item?item_id=100',
-                         'title':'Buy Item'
-                       },
-                       {
-                         'type':'postback',
-                         'title':'Bookmark Item',
-                         'payload':'USER_DEFINED_PAYLOAD_FOR_ITEM100'
-                       }
-                     ]
-                   },
-                   {
-                     'title':'Classic Grey T-Shirt',
-                     'image_url':'http://petersapparel.parseapp.com/img/item101-thumb.png',
-                     'subtitle':'Soft gray cotton t-shirt is back in style',
-                     'buttons':[
-                       {
-                         'type':'web_url',
-                         'url':'https://petersapparel.parseapp.com/view_item?item_id=101',
-                         'title':'View Item'
-                       },
-                       {
-                         'type':'web_url',
-                         'url':'https://petersapparel.parseapp.com/buy_item?item_id=101',
-                         'title':'Buy Item'
-                       },
-                       {
-                         'type':'postback',
-                         'title':'Bookmark Item',
-                         'payload':'USER_DEFINED_PAYLOAD_FOR_ITEM101'
-                       }
-                     ]
-                   }
-                 ]
-               }
-        }
-    });
+// reply to any incoming message
+controller.on('message_received', function(bot, message) {
+    bot.reply(message, 'I heard... something!');
 });
