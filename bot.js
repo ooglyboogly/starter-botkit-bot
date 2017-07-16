4dea22ae046ffb52ba1f7364cd3fcff1765ded32
ooglyboogly <drlucas17@yahoo.com>
2017-07-15 20:45:08 -0500

    addHears()

--- bot.js
+++ bot.js
@@ -49,8 +49,32 @@ fs.readdirSync(normalizedPath).forEach(file => {
 // You can tie into the execution of the script using the functions
 // controller.studio.before, controller.studio.after and controller.studio.validate
 if (STUDIO_TOKEN) {
-  controller.on('direct_message,direct_mention,mention'
-, (bot, message) => {
+  controller.on('direct_message,direct_mention,mention', (bot, message) => {
+    controller.studio.runTrigger(bot, message.text, message.user, message.channel).then(convo => {
+      if (!convo) {
+        // no trigger was matched
+        // If you want your bot to respond to every message,
+        // define a 'fallback' script in Botkit Studio
+        // and uncomment the line below.
+        // controller.studio.run(bot, 'fallback', message.user, message.channel)
+      } else {
+        // set variables here that are needed for EVERY script
+        // use controller.studio.before('script') to set variables specific to a script
+        convo.setVar('current_time', new Date())
+      }
+    }).catch(err => {
+      bot.reply(message, 'I experienced an error with a request to Botkit Studio: ' + err)
+      debug('Botkit Studio: ', err)
+    })
+  })
+} else {
+  console.log('~~~~~~~~~~')
+  console.log('NOTE: Botkit Studio functionality has not been enabled')
+  console.log('To enable, pass in a studio_token parameter with a token from https://studio.botkit.ai/')
+}
+
+if (STUDIO_TOKEN) {
+  controller.hears(['keyword','\w\w\w\w+?\.\w\w\w\w+?\.\w\w\w\w+'],['message_received'],function(bot,message) {
     controller.studio.runTrigger(bot, message.text, message.user, message.channel).then(convo => {
       if (!convo) {
         // no trigger was matched
