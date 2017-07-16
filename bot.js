b8f1e5d9c53353859ecc2776f4f7e63c49283d65
ooglyboogly <drlucas17@yahoo.com>
2017-07-15 21:26:10 -0500

    revert

--- bot.js
+++ bot.js
@@ -42,7 +42,33 @@ fs.readdirSync(normalizedPath).forEach(file => {
   require(path.join(normalizedPath, file))(controller)
 })
 
-// reply to any incoming message
-controller.on('message_received', function(bot, message) {
-    bot.reply(message, 'I heard... something!');
-});
+// This captures and evaluates any message sent to the bot as a DM
+// or sent to the bot in the form "@bot message" and passes it to
+// Botkit Studio to evaluate for trigger words and patterns.
+// If a trigger is matched, the conversation will automatically fire!
+// You can tie into the execution of the script using the functions
+// controller.studio.before, controller.studio.after and controller.studio.validate
+if (STUDIO_TOKEN) {
+  controller.on('direct_message,direct_mention,mention', (bot, message) => {
+    controller.studio.runTrigger(bot, message.text, message.user, message.channel).then(convo => {
+      if (!convo) {
+        // no trigger was matched
+        // If you want your bot to respond to every message,
+        // define a 'fallback' script in Botkit Studio
+        // and uncomment the line below.
+        // controller.studio.run(bot, 'fallback', message.user, message.channel)
+      } else {
+        // set variables here that are needed for EVERY script
+        // use controller.studio.before('script') to set variables specific to a script
+        convo.setVar('current_time', new Date())
+      }
+    }).catch(err => {
+      bot.reply(message, 'I experienced an error with a request to Botkit Studio: ' + err)
+      debug('Botkit Studio: ', err)
+    })
+  })
+} else {
+  console.log('~~~~~~~~~~')
+  console.log('NOTE: Botkit Studio functionality has not been enabled')
+  console.log('To enable, pass in a studio_token parameter with a token from https://studio.botkit.ai/')
+}
