a4fa87b034172dc2e6b0aafcaf2e52b13406487f
ooglyboogly <drlucas17@yahoo.com>
2017-07-15 20:36:26 -0500

    revert

--- bot.js
+++ bot.js
@@ -49,7 +49,8 @@ fs.readdirSync(normalizedPath).forEach(file => {
 // You can tie into the execution of the script using the functions
 // controller.studio.before, controller.studio.after and controller.studio.validate
 if (STUDIO_TOKEN) {
-  controller.on('message_received', (bot, message) => {
+  controller.on('direct_message,direct_mention,mention'
+, (bot, message) => {
     controller.studio.runTrigger(bot, message.text, message.user, message.channel).then(convo => {
       if (!convo) {
         // no trigger was matched
