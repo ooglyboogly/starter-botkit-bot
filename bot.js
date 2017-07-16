f9d729727857b6a5de3c8ac4f8afe9ca4fa0c569
ooglyboogly <drlucas17@yahoo.com>
2017-07-15 21:03:53 -0500

    simplify

--- bot.js
+++ bot.js
@@ -50,23 +50,7 @@ fs.readdirSync(normalizedPath).forEach(file => {
 // controller.studio.before, controller.studio.after and controller.studio.validate
 if (STUDIO_TOKEN) {
   controller.on('direct_message,direct_mention,mention', (bot, message) => {
-    controller.studio.runTrigger(bot, message.text, message.user, message.channel).then(convo => {
-      if (!convo) {
-        // no trigger was matched
-        // If you want your bot to respond to every message,
-        // define a 'fallback' script in Botkit Studio
-        // and uncomment the line below.
-        // controller.studio.run(bot, 'fallback', message.user, message.channel)
-      } else {
-        // set variables here that are needed for EVERY script
-        // use controller.studio.before('script') to set variables specific to a script
-        convo.setVar('current_time', new Date())
-      }
-    }).catch(err => {
-      bot.reply(message, 'I experienced an error with a request to Botkit Studio: ' + err)
-      debug('Botkit Studio: ', err)
-    })
-  })
+      bot.reply(message, 'I experienced an error with a request to Botkit Studio:')
 } else {
   console.log('~~~~~~~~~~')
   console.log('NOTE: Botkit Studio functionality has not been enabled')
