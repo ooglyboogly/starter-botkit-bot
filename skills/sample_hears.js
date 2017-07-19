0e9df3d9057e4225c437b6c66c62519d30c5f523
ooglyboogly <drlucas17@yahoo.com>
2017-07-18 23:32:32 -0500

    subby

--- skills/sample_hears.js
+++ skills/sample_hears.js
@@ -131,7 +131,9 @@ controller.hears(['Tyranitar'], 'ambient', (bot, message) => {
 	function evaluate () {
 		if (whochannel2 == "testchannelpublic"  && whodis2 == "ooglybooglies"){
 			var callout = callouts[Math.floor(Math.random()*callouts.length)];
-			var coords = message.substring(message.indexOf("#")+1);
+						var coords = message.substring(5);
+
+			//var coords = message.substring(message.indexOf("#")+1);
 			//coords = coords.substring(coords.indexOf("");
 			var callout = callout+" Trex is located at ";
 			bot.reply(message, coords);
