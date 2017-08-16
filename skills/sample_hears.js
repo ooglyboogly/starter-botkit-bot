2c0c39220fb9b565246e310e6557fadb6fda0ba5
ooglyboogly <drlucas17@yahoo.com>
2017-08-15 21:34:32 -0500

    find the error

--- skills/sample_hears.js
+++ skills/sample_hears.js
@@ -210,6 +210,7 @@ controller.hears(['embed: A wild'], 'ambient', (bot, message) => {
 	var callouts = [""];
 	//https://slack.com/api/chat.postMessage?token="+XOXP_API_KEY+"&channel=%23gymalert&text=Tester&pretty=1
 	function getUserAndChannel(callback){
+		bot.reply(message,message.match);
 		bot.api.users.info({user: message.user}, function(err, info){
 			whodisid2 = message.user;
 			whodis2 = info.user.name;
